3214b6dc761870679579a27a453b2e41032a2075
Shubham Daulkar <daulkarshubham@gmail.com>
2024-02-08 20:38:59 +0530

    appwrite_setup_completed

---
+++ blog_reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import conf from './conf/conf';
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+
+      <h1>Blog </h1>
+    </>
+  )
+}
+
+export default App
